75325db645cf9cee134af405b7de96382097b6cf
bjornKhermik10 <bjorn.khermik@helsinki.fi>
2025-11-10 09:07:45 +0200

    Clean up

--- viikko1/nhl-statistics-1/src/statistics_service.py
+++ viikko1/nhl-statistics-1/src/statistics_service.py
@@ -1,14 +1,16 @@
 from player_reader import PlayerReader
 
 class StatisticsService:
-    def __init__(self):
-        reader = PlayerReader()
+    def __init__(self, player_reader):
+        reader = player_reader
+
         self._players = reader.get_players()
 
     def search(self, name):
         for player in self._players:
             if name in player.name:
                 return player
+
         return None
 
     def team(self, team_name):
@@ -16,6 +18,7 @@ class StatisticsService:
             lambda player: player.team == team_name,
             self._players
         )
+
         return list(players_of_team)
 
     def top(self, how_many):
