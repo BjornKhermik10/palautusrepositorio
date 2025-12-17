0ca65756963e266e07596b0ea9fe7aa1f392d5d7
bjornKhermik10 <bjorn.khermik@helsinki.fi>
2025-12-17 11:33:52 +0200

    th7 tehty

--- viikko7/src/index.py
+++ viikko7/src/index.py
@@ -1,7 +1,4 @@
-from kps_pelaaja_vs_pelaaja import KPSPelaajaVsPelaaja
-from kps_tekoaly import KPSTekoaly
-from kps_parempi_tekoaly import KPSParempiTekoaly
-
+from peli_tehdas import luo_peli
 
 def main():
     while True:
@@ -10,34 +7,16 @@ def main():
               "\n (b) Tekoälyä vastaan"
               "\n (c) Parannettua tekoälyä vastaan"
               "\nMuilla valinnoilla lopetetaan"
-              )
+        )
 
         vastaus = input()
+        peli = luo_peli(vastaus)
 
-        if vastaus.endswith("a"):
-            print(
-                "Peli loppuu kun pelaaja antaa virheellisen siirron eli jonkun muun kuin k, p tai s"
-            )
-
-            kaksinpeli = KPSPelaajaVsPelaaja()
-            kaksinpeli.pelaa()
-        elif vastaus.endswith("b"):
-            print(
-                "Peli loppuu kun pelaaja antaa virheellisen siirron eli jonkun muun kuin k, p tai s"
-            )
-
-            yksinpeli = KPSTekoaly()
-            yksinpeli.pelaa()
-        elif vastaus.endswith("c"):
-            print(
-                "Peli loppuu kun pelaaja antaa virheellisen siirron eli jonkun muun kuin k, p tai s"
-            )
-
-            haastava_yksinpeli = KPSParempiTekoaly()
-            haastava_yksinpeli.pelaa()
+        if peli:
+            print("Peli alkaa. Pelin voi lopettaa syöttämällä jotain muuta kuin k, p tai s.")
+            peli.pelaa()
         else:
             break
-
 
 if __name__ == "__main__":
     main()
